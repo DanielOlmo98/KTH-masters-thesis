573ce8543c4fdfe8196d028dbbed6d5c1fd79823
dolmo <dolmo@kth.se>
2022-03-25 19:12:06 +0100

    aug train

--- dl/unet_test.py
+++ dl/unet_test.py
@@ -5,13 +5,95 @@ import json
 from dl.dataloader import KFoldValLoaders, CamusDatasetPNG
 import os
 from unet_model import Unet
-from unet_training import evaluate_unet, save_metrics
+import pandas as pd
+import numpy as np
 
 
 def load_unet(filename, output_ch, levels, top_feature_ch):
     saved_unet = Unet(output_ch=output_ch, levels=levels, top_feature_ch=top_feature_ch)
     saved_unet.load_state_dict(torch.load(filename))
     return saved_unet.cuda()
+
+
+def evaluate_unet(unet, val_loader, val_metrics):
+    unet.eval()
+    val_loader = iter(val_loader)
+    next_batch = next(val_loader)
+    n_classes = next_batch[1].size()[1]
+    metric_lists_ES = [[] for _ in range(n_classes)]  # one list per class
+    metric_lists_ED = [[] for _ in range(n_classes)]  # one list per class
+    try:
+        while True:
+            with torch.no_grad():
+                img, seg, ED_or_ES = next_batch
+                prediction = torch.softmax(unet(img), dim=1)
+
+            for i in range(img.shape[0]):
+                for n in range(n_classes):
+                    # append 1x3 tensor per class containing precision recall and f1 for the class
+                    if ED_or_ES[i] == 1:
+                        metric_lists_ED[n].append(dl.metrics.get_f1_metrics(prediction[:, n, :, :], seg[:, n, :, :]))
+                    elif ED_or_ES[i] == 2:
+                        metric_lists_ES[n].append(dl.metrics.get_f1_metrics(prediction[:, n, :, :], seg[:, n, :, :]))
+
+            next_batch = next(val_loader)
+
+    except StopIteration:
+        metric_lists_ED = torch.FloatTensor(metric_lists_ED)
+        metric_lists_ES = torch.FloatTensor(metric_lists_ES)
+        for metric_list, val_m_dict_key in zip([metric_lists_ED, metric_lists_ES], ['ED', 'ES']):
+            p_list = []
+            r_list = []
+            f1_list = []
+            for n in range(n_classes):
+                p_list.append(torch.mean(metric_list[n, :, 0]))
+                r_list.append(torch.mean(metric_list[n, :, 1]))
+                f1_list.append(torch.mean(metric_list[n, :, 2]))
+            val_metrics[val_m_dict_key]['p'].append(p_list)
+            val_metrics[val_m_dict_key]['r'].append(r_list)
+            val_metrics[val_m_dict_key]['f1'].append(f1_list)
+        pass
+
+
+def save_metrics(savename, val_metrics_ES_and_ED):
+    m_frames = []
+    for val_metrics in val_metrics_ES_and_ED.values():
+        fold_arrays = []
+        p_tensor = torch.tensor(val_metrics['p'])
+        r_tensor = torch.tensor(val_metrics['r'])
+        f1_tensor = torch.tensor(val_metrics['f1'])
+        folds, n_classes = p_tensor.shape
+        for fold in range(folds):
+            fold_arrays.append(np.stack((p_tensor[fold], r_tensor[fold], f1_tensor[fold])))
+
+        row_idxs = pd.MultiIndex.from_product(
+            [range(folds), list(val_metrics.keys())],
+            names=['fold', 'metric']
+        )
+        col_sub_idxs = pd.MultiIndex.from_product([range(n_classes)], names=['class'])
+        metrics_frame = pd.DataFrame(np.vstack(fold_arrays), index=row_idxs, columns=col_sub_idxs)
+        avgs = calc_metric_avgs(metrics_frame, list(val_metrics.keys()))
+        m_frames.append(pd.concat([metrics_frame, avgs]))
+
+    metrics_frame_full = pd.concat(m_frames, keys=list(val_metrics_ES_and_ED.keys()), axis=1)
+    metrics_frame_full.to_csv(f'{savename}metrics.csv')
+    return metrics_frame_full
+
+
+def calc_metric_avgs(metrics_frame, metrics_name):
+    avgs = []
+    f1_std = []
+    for metric in metrics_name:
+        avgs.append(metrics_frame.xs(metric, level=1).mean())
+        if metric == 'f1':
+            f1_std.append(metrics_frame.xs(metric, level=1).std())
+
+    metrics_name.append('f1_std_dev')
+    row_idxs = pd.MultiIndex.from_product(
+        [['avg'], metrics_name],
+        names=['fold', 'metric']
+    )
+    return pd.DataFrame([*avgs, *f1_std], index=row_idxs)
 
 
 def check_predictions(unet, val_loader, n_images):
@@ -76,10 +158,11 @@ def eval_test_set(unet, net_name):
 
 
 if __name__ == '__main__':
-    path = 'train_results/camus_png/unet_5levels_augment_False_16top/fold_0.pt'
+    # path = 'train_results/camus_png/unet_5levels_augment_False_16top/fold_0.pt'
     # unet = load_unet(path, out_channels=4, levels=5, top_ch=64)
     # val_loaders = KFoldValLoaders(CamusDatasetPNG(), split=8)
     # check_predictions(unet, val_loaders[0], n_images=1)
-    val_folds('camus_png/unet_5levels_augment_True_32top')
+    for net_folder in os.listdir('camus_png'):
+        val_folds(f'camus_png/{net_folder}')
     print()
     # eval_test_set(unet)
--- dl/unet_training.py
+++ dl/unet_training.py
@@ -18,7 +18,7 @@ from torch.profiler import profile, record_function, ProfilerActivity
 from pynvml.smi import nvidia_smi
 
 
-def train_loop(unet, train_loader, val_loader, savename, val_metrics, epochs, optimizer, loss_func, **kwargs):
+def train_loop(unet, train_loader, val_loader, savename, epochs, optimizer, loss_func, **kwargs):
     bar = Fore.WHITE + '{l_bar}{bar}' + Fore.WHITE + '| {n_fmt}/{total_fmt} [{elapsed}{postfix}]'
     bar_val = Fore.GREEN + '{l_bar}{bar}' + Fore.GREEN + '| {n_fmt}/{total_fmt} [{elapsed}  {postfix}]'
     val_min_loss = 9
@@ -59,8 +59,6 @@ def train_loop(unet, train_loader, val_loader, savename, val_metrics, epochs, op
             train_loader.dataset.join_queues()
 
     utils.plot_losses(train_loss_list, val_loss_list, filename=f'{savename}_loss.png')
-    if val_loader is not None:
-        evaluate_unet(unet, val_loader, val_metrics)
 
 
 def train(optimizer, loss_func, train_loop, unet):
@@ -102,92 +100,10 @@ def val(loss_func, val_loop, unet):
     return val_running_loss / val_loop.n
 
 
-def evaluate_unet(unet, val_loader, val_metrics):
-    unet.eval()
-    val_loader = iter(val_loader)
-    next_batch = next(val_loader)
-    n_classes = next_batch[1].size()[1]
-    metric_lists_ES = [[] for _ in range(n_classes)]  # one list per class
-    metric_lists_ED = [[] for _ in range(n_classes)]  # one list per class
-    try:
-        while True:
-            with torch.no_grad():
-                img, seg, ED_or_ES = next_batch
-                prediction = torch.softmax(unet(img), dim=1)
-
-            for i in range(img.shape[0]):
-                for n in range(n_classes):
-                    # append 1x3 tensor per class containing precision recall and f1 for the class
-                    if ED_or_ES[i] == 1:
-                        metric_lists_ED[n].append(dl.metrics.get_f1_metrics(prediction[:, n, :, :], seg[:, n, :, :]))
-                    elif ED_or_ES[i] == 2:
-                        metric_lists_ES[n].append(dl.metrics.get_f1_metrics(prediction[:, n, :, :], seg[:, n, :, :]))
-
-            next_batch = next(val_loader)
-
-    except StopIteration:
-        metric_lists_ED = torch.FloatTensor(metric_lists_ED)
-        metric_lists_ES = torch.FloatTensor(metric_lists_ES)
-        for metric_list, val_m_dict_key in zip([metric_lists_ED, metric_lists_ES], ['ED', 'ES']):
-            p_list = []
-            r_list = []
-            f1_list = []
-            for n in range(n_classes):
-                p_list.append(torch.mean(metric_list[n, :, 0]))
-                r_list.append(torch.mean(metric_list[n, :, 1]))
-                f1_list.append(torch.mean(metric_list[n, :, 2]))
-            val_metrics[val_m_dict_key]['p'].append(p_list)
-            val_metrics[val_m_dict_key]['r'].append(r_list)
-            val_metrics[val_m_dict_key]['f1'].append(f1_list)
-        pass
-
-
-def save_metrics(savename, val_metrics_ES_and_ED):
-    m_frames = []
-    for val_metrics in val_metrics_ES_and_ED.values():
-        fold_arrays = []
-        p_tensor = torch.tensor(val_metrics['p'])
-        r_tensor = torch.tensor(val_metrics['r'])
-        f1_tensor = torch.tensor(val_metrics['f1'])
-        folds, n_classes = p_tensor.shape
-        for fold in range(folds):
-            fold_arrays.append(np.stack((p_tensor[fold], r_tensor[fold], f1_tensor[fold])))
-
-        row_idxs = pd.MultiIndex.from_product(
-            [range(folds), list(val_metrics.keys())],
-            names=['fold', 'metric']
-        )
-        col_sub_idxs = pd.MultiIndex.from_product([range(n_classes)], names=['class'])
-        metrics_frame = pd.DataFrame(np.vstack(fold_arrays), index=row_idxs, columns=col_sub_idxs)
-        avgs = calc_metric_avgs(metrics_frame, list(val_metrics.keys()))
-        m_frames.append(pd.concat([metrics_frame, avgs]))
-
-    metrics_frame_full = pd.concat(m_frames, keys=list(val_metrics_ES_and_ED.keys()), axis=1)
-    metrics_frame_full.to_csv(f'{savename}val_metrics.csv')
-    return metrics_frame_full
-
-
-def calc_metric_avgs(metrics_frame, metrics_name):
-    avgs = []
-    f1_std = []
-    for metric in metrics_name:
-        avgs.append(metrics_frame.xs(metric, level=1).mean())
-        if metric == 'f1':
-            f1_std.append(metrics_frame.xs(metric, level=1).std())
-
-    metrics_name.append('f1_std_dev')
-    row_idxs = pd.MultiIndex.from_product(
-        [['avg'], metrics_name],
-        names=['fold', 'metric']
-    )
-    return pd.DataFrame([*avgs, *f1_std], index=row_idxs)
-
-
 def kfold_train_unet(unet, foldername, train_settings, dataloader_settings, **kwargs):
     """
     Train unet and calculate metrics in k-folds. kwargs are ignored.
     """
-    val_metrics = {'ED': {'p': [], 'r': [], 'f1': []}, 'ES': {'p': [], 'r': [], 'f1': []}}
 
     if dataloader_settings['augments'] is True:
         dataloader_settings['augments'] = get_transforms(**aug_settings)
@@ -208,7 +124,7 @@ def kfold_train_unet(unet, foldername, train_settings, dataloader_settings, **kw
         path = f'{foldername}fold_{fold}'
         print(f'Fold #{fold}')
 
-        train_loop(unet, train_loader, val_loader, path, val_metrics, **train_settings)
+        train_loop(unet, train_loader, val_loader, path, **train_settings)
         del train_loader.dataset.aug_imgs, train_loader.dataset.aug_segs
         del val_loader.dataset.aug_imgs, val_loader.dataset.aug_segs
         del train_loader, val_loader
@@ -217,16 +133,8 @@ def kfold_train_unet(unet, foldername, train_settings, dataloader_settings, **kw
         gc.collect()
         torch.cuda.empty_cache()
 
-    metrics_frame = save_metrics(foldername, val_metrics)
-    print('ED')
-    print(metrics_frame.xs('avg').xs('ED', axis=1))
-    print('\nES')
-    print(metrics_frame.xs('avg').xs('ES', axis=1))
-
 
 def full_train_unet(unet, foldername, train_settings, dataloader_settings, **kwargs):
-    val_metrics = {'ED': {'p': [], 'r': [], 'f1': []}, 'ES': {'p': [], 'r': [], 'f1': []}}
-
     if dataloader_settings['augments'] is True:
         dataloader_settings['augments'] = get_transforms(**aug_settings)
     else:
@@ -240,7 +148,7 @@ if __name__ == '__main__':
 
     unet_settings = {
         'levels': 5,
-        'top_feature_ch': 32,
+        'top_feature_ch': 64,
         'output_ch': 4
     }
     unet = Unet(**unet_settings).cuda()
@@ -268,7 +176,7 @@ if __name__ == '__main__':
         "batch_size": 8,
         "split": 8,
         "dataset": CamusDatasetPNG(dataset=dataset),
-        "augments": True,
+        "augments": False,
         "n_train_aug_threads": 2,
     }
 
@@ -293,7 +201,9 @@ if __name__ == '__main__':
     ''' TODO
         - change aug params
         - put us sim img gt though coord transform
-        - plot CNN
         - run 4 level 64 top and 5 level 64 top
         - denoise and then noise
+        - avg LA and stddev
+        - run val on all
+        - train with different weights?
     '''
